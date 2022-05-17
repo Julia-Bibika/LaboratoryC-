d24661b6e05e88f9575cf6f4b15fd3f747200d1c
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2022-05-17 20:07:26 +0300

    Labka1 C#

---
+++ Завдання 1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labka1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c=");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double min1 = Min1int(a, b, c);
+            Console.WriteLine(min1);
+            double min = Minint(a, b, c);
+            Console.WriteLine(min);
+            double min2 = Min2int(a, b, c);
+            Console.WriteLine(min2);
+            double y = min1 + min + min2;
+            Console.WriteLine("Y = {0}", y);
+        }
+        static double Min1int(double a, double b, double c)
+        {
+            if (a - (Math.Pow(a, 2)) < (c - (Math.Pow(c, 2))))
+            {
+                return (a - (Math.Pow(a, 2)));
+            }
+            else
+            {
+                return (c - (Math.Pow(c, 2)));
+            }
+        }
+        static double Minint(double a, double b, double c)
+        {
+            if ((Math.Pow(a, 3) - Math.Pow(b, 3)) < (Math.Pow(a, 3)))
+            {
+                return (Math.Pow(a, 3) - Math.Pow(b, 3));
+            }
+            else
+            {
+                return (Math.Pow(a, 3));
+            }
+        }
+        static double Min2int(double a, double b, double c)
+        {
+            if ((Math.Pow(a, 2) + 3 * b - 4) < (Math.Pow(c, 2) + 2 * c + 1))
+            {
+                return (Math.Pow(a, 2) - Math.Pow(b, 2));
+            }
+            else
+            {
+                return (Math.Pow(b, 2) + Math.Pow(c, 2));
+            }
+        }
+    }
+}
---
+++ Завдання 2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labka1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Z2());
+        }
+        static string Z2()
+        {
+            Console.WriteLine("Zavd 2");
+            Console.Write("number = ");
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = new int[4];
+            int numb = number;
+            for (int i = 3; i >= 0; i--)
+            {
+                numbers[i] = number % 10;
+                number = number / 10;
+            }
+            number = numb;
+            int max = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (number % 10 > max)
+                    max = number % 10;
+                number = number / 10;
+            }
+
+            number = numb;
+
+
+            int min = 9;
+            for (int i = 0; i < 4; i++)
+            {
+                if (number % 10 < min)
+                    min = number % 10;
+                number = number / 10;
+            }
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (numbers[i] == min)
+                {
+                    numbers[i] = max;
+                    i++;
+
+                }
+                if (numbers[i] == max)
+                {
+                    numbers[i] = min;
+                    i++;
+
+                }
+            }
+
+
+            return String.Join("", numbers);
+        }
+    }
+}
